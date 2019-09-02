7e22371d930863895379394a5b75b6ed730048a9
MazX0p <54814433+MazX0p@users.noreply.github.com>
2019-09-02 20:00:55 +0300

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,15 @@
+###############################################
+#                                             #
+#            BackUp Script by Maz0zp          #
+#                                             #
+###############################################
+
+backup_file="/root/Desktop"
+dest="/root/Desktop"
+archive_file="backup.tgz"
+if [ -e $dest/$archive_file];
+	then
+		rm -rf $archive_file
+	else
+		tar czf $dest/$archive_file $backup_file
+fi >/dev/null 2>&1
